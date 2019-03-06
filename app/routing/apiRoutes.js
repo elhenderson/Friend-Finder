ee1f1287d3b4baff78223c98b08cbfcf67c5290f
elhenderson <evann.henderson2@yahoo.com>
2019-03-05 18:15:55 -0600

    pull

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("hello");
-})
-
 var friends = 
 [
     {
@@ -25,6 +21,11 @@ var friends =
     }
 ]
 
+
+
+router.get('/', (req, res) => {
+    res.json(friends);
+})
 
 
 router.post('/', (req, res) => {
